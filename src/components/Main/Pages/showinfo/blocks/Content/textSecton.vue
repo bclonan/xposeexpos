a01eb0071f33136c92f1380215d9a192fa40179b
possibly1 <brad.clonan@hotmail.com>
2018-11-06 13:33:20 -0500

    textsection

---
+++ src/components/Main/Pages/showinfo/blocks/Content/textSecton.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="section text-centered">
+    <p :style="[contentStyle]" :class="contentClass">
+      {{content}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'textSection'
+  };
+</script>
+
+<style scoped>
+</style>
